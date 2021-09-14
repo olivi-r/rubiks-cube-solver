9834c4b4444b3ff448cfc29b488bb8dd82498f4f
James Ryan <r.james.dev@gmail.com>
2021-09-14 13:09:58 +0100

    added 3D vectors for 3d maths

--- math3d.py
+++ math3d.py
@@ -22,3 +22,35 @@ class Matrix3x3:
 
         # raised when multiplying with wrong types
         return NotImplemented
+
+
+class Vector3(Matrix3x3):
+    def __init__(self, i: float=0, j: float=0, k: float=0):
+        # Vectors are represented as matrices to allow easy matrix - vector multiplication
+        assert isinstance(i, (int, float))
+        assert isinstance(j, (int, float))
+        assert isinstance(k, (int, float))
+        self.data = [[i, 0, 0], [j, 0, 0], [k, 0, 0]]
+
+    @property
+    def i(self):
+        return self.data[0][0]
+
+    @i.setter
+    def i(self, value):
+        self.data[0][0] = value
+
+    @property
+    def j(self):
+        return self.data[1][0]
+
+    @j.setter
+    def j(self, value):
+        self.data[1][0] = value
+    @property
+    def k(self):
+        return self.data[2][0]
+
+    @k.setter
+    def k(self, value):
+        self.data[2][0] = value
