9f0d30086f390e475043b76eee95c11854040607
Jay Ryan <jay.p.ryan.2003@gmail.com>
2022-04-29 12:18:29 +0100

    Updated build script

--- build.bat
+++ build.bat
@@ -1,2 +1,6 @@
 @echo off
 pyinstaller screen.py -n "Rubiks Cube Solver" --clean -w -F -i exe_logo.ico --add-data logo.png;.
+move "dist\Rubiks Cube Solver.exe" "Rubiks Cube Solver.exe"
+del "Rubiks Cube Solver.spec"
+rmdir /S /Q build
+rmdir dist
